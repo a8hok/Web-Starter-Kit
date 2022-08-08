df3d073c385d4fb3ecc28adac8eda6bad5c40765
mukesh kumar <mukeshkumar130803@gmail.com>
2022-08-08 11:19:21 +0530

    sticky navbar in home.js

--- portfolio/script/home.js
+++ portfolio/script/home.js
@@ -11,3 +11,44 @@ navButton.addEventListener('click',()=>{
     switchImg=curentImg;
     curentImg=temp;
 });
+
+
+
+//! stiky nav-bar
+const head=document.querySelector("header");
+const navBar=document.querySelector("nav");
+const observeNav=new IntersectionObserver((ent,obs)=>{
+    ent.forEach((element)=>{
+        if(element.isIntersecting){
+            // console.log("true");
+            navBar.style.position="static";
+        }
+        else{
+            // console.log("false");
+            navBar.style.position="sticky";
+        }
+    })
+},{root:null,threshold:0.3});
+observeNav.observe(head);
+
+
+
+//! graph AnimationEffect
+const bar=document.querySelectorAll(".bar");
+bar.forEach((ele)=>{
+    let insial=0;
+    let final=getComputedStyle(ele).getPropertyValue("--i");
+    let inc=final/100;
+    let barWidth=window.getComputedStyle(ele,":before");
+    function animat(){
+        console.log(insial);
+        barWidth["width"]=`${insial}%`;
+        insial+=inc;
+        if(insial<=final){
+            setTimeout(10,animat());
+        }
+        return;
+    }
+    animat();
+    
+});
